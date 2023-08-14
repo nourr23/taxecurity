a877a12645ee211cebc11d25f53dc17bdd8fb8ef
nour <nour.edaher.jouini@superverse.tech>
2023-08-14 11:07:17 +0100

    starting dashbord

---
+++ packages/dashboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-yellow">
+      <div className='text-red-800 text-xl font-bold'>
+        test
+      </div>
+    </div>
+  );
+}
+
+export default App;
