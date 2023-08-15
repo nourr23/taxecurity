8bac6d4558201d8eaccdc81b957719244952e5c9
nour <nour.edaher.jouini@superverse.tech>
2023-08-15 18:47:14 +0100

    adding header without icons

---
+++ packages/dashboard/src/components/header/index.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="h-[90px] w-full flex px-6 items-center justify-between border-b-2">
+      <div>
+        Mohamed Nour Jouini
+      </div>
+      <div>
+        notification Icon and logout
+      </div>
+    </div>
+  );
+};
+export default Header;
