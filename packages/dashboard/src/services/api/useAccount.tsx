b31f18f05c3a922f0268e142aeedc5cf8acb74c0
nour <nour.edaher.jouini@superverse.tech>
2023-11-22 16:04:56 +0100

    fixing expired token

--- packages/dashboard/src/services/api/useAccount.tsx
+++ packages/dashboard/src/services/api/useAccount.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "../global/apiClient";
 import { getItem } from "../../core/storage";
+// import { useAuth } from "../../core/auth";
+import { useAuth } from "../../core/auth";
 
 export const getMe = async () => {
   const data = await client.get("admin/workers/me", {
@@ -14,8 +16,14 @@ export const getMe = async () => {
 };
 
 export function useAccount() {
+  const { signOut } = useAuth();
   return useQuery<any>(["me"], () => getMe(), {
     // retry: true,
     keepPreviousData: true,
+    onError: (error: any) => {
+      if (error.response.data.statusCode === 401) {
+        signOut();
+      }
+    },
   });
 }
